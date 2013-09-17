6257460f7f1b2fed9af60b076f3deff2c9172e46
ubuntu <ubuntu@lubuntu-vm.(none)>
2013-09-17 08:57:06 -0400

    conversion script

--- md2txt.sh
+++ md2txt.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# usage: bash md2txt.sh accounts.js mules.txt
+# uses dos2unix
+
 dos2unix $1 && sed "s/\"//g" $1 | sed "s/'//g" | sed "s/\://g" | sed "s/\,//g" | sed "s/var accounts = {//g" | sed "s/}//g" | sed '/^$/d' > $2
